e978dce9d96b6dfb151c35074b2c068cda2938ae
ccpu <m.doaie@hotmail.com>
2025-08-12 12:10:13 +0100

    fix: update package generator import path to correct module

--- turbo/generators/config.ts
+++ turbo/generators/config.ts
@@ -1,5 +1,6 @@
-import { packageGenerator } from '@pixpilot/pnpm-workspace-package-generator';
+import { packageGenerator } from '@pixpilot/workspace-package-generator';
 
 module.exports = function generator(plop: unknown) {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   packageGenerator(plop);
 };
