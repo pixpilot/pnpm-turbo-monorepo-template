c0584f867df9696d7018542fb1c6e3a89a0e0beb
ccpu <m.doaie@hotmail.com>
2025-08-14 21:57:11 +0100

    chore: update @pixpilot/dev-config and @pixpilot/eslint-config versions
    - Bump @pixpilot/dev-config from ^2.1.0 to ^3.0.0 in pnpm-workspace.yaml
    - Update @pixpilot/eslint-config from ^2.3.0 to ^2.4.3 in tooling/eslint/package.json

--- tooling/eslint/base.js
+++ tooling/eslint/base.js
@@ -1,22 +1,11 @@
 import makeConfig from '@pixpilot/eslint-config';
-import turboPlugin from 'eslint-plugin-turbo';
 
 /**
  * @type {ReturnType<typeof makeConfig>}
  */
-const baseConfig = makeConfig(
-  {
-    typescript: {
-      tsconfigPath: './tsconfig.json',
-    },
-    pnpm: false,
-  },
-  {
-    files: ['**/*.js', '**/*.ts', '**/*.tsx'],
-    plugins: {
-      turbo: turboPlugin,
-    },
-  },
-);
+const baseConfig = makeConfig({
+  pnpm: false,
+  turbo: true,
+});
 
 export default baseConfig;
